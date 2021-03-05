de4d5dcd760a4da9c344741e820b1b5745086a9e
Camilo Barreiro <1845@holbertonschool.com>
2021-03-04 21:51:44 -0500

    Add complete strategy

--- Back-End/Ichimoku.py
+++ Back-End/Ichimoku.py
@@ -4,30 +4,58 @@ from datetime import datetime
 from graph import Graph
 
 
-def ichimoku(Data):
-    Index = Data.loc[datetime(2019, 3, 1), 'index']
-    Lenght = len(Data.loc[datetime(2019, 3, 1):datetime.now(), 'High'])
-    Data.index = Data['index']
-    Data.drop(['index'], axis=1, inplace=True)
-    # Tenkan
-    j = 0
-    for i in range(Lenght):
-        Data.loc[Index + j, 'Tenkan'] = (Data.iloc[(Index - 9) + j:Index + j, 2].max() + Data.iloc[
-                                                                                         (Index - 9) + j:Index + j,
-                                                                                         3].min()) / 2
-        j += 1
-    # Kijun-sen
-    j = 0
-    for i in range(Lenght):
-        Data.loc[Index + j, 'Kijun-sen'] = (Data.iloc[(Index - 26) + j:Index + j, 2].max() + Data.iloc[
-                                                                                             (Index - 26) + j:Index + j,
-                                                                                             3].min()) / 2
-        j += 1
-    Data.rename(columns={'Close': 'Price'}, inplace=True)
+def ichimoku(Data, Tenkan_Period, Kijun_sen_Period, Chikou_span_Period,  Senkou_span_A_Period, Senkou_span_B_Period, Shift_Senkou_span_B):
+    for i in range(len(Data)):
+        try:
+            # Tenkan trend
+            if (i + Tenkan_Period) <= len(Data) - 1:
+                Data.loc[(i + Tenkan_Period), 'Tenkan'] = (Data.iloc[i:(i + Tenkan_Period), 2].max() + Data.iloc[i:(i + Tenkan_Period), 3].min()) / 2
+            # Kijun-sen trend
+            if (i + Kijun_sen_Period) <= len(Data) - 1:
+                Data.loc[(i + Kijun_sen_Period), 'Kijun-sen'] = (Data.iloc[i:(i + Kijun_sen_Period), 2].max() + Data.iloc[i:(i + Kijun_sen_Period), 3].min()) / 2
+        except ValueError:
+            pass
+
+    for i in range(len(Data)):
+        try:
+            # Chikou-span
+            if i + Chikou_span_Period <= len(Data) - 1:
+                Data.loc[i, 'Chikou-span'] = Data.iloc[(i + Chikou_span_Period), 4]
+            # Senkou-span A
+            if (i + (Senkou_span_A_Period+Kijun_sen_Period)) <= len(Data) - 1:
+                Data.loc[(i + (Senkou_span_A_Period+Kijun_sen_Period)), 'Senkou-span A'] = (Data.iloc[(i + Kijun_sen_Period), 6] + Data.iloc[(i + Kijun_sen_Period), 7]) / 2
+            # Senkou-span B
+            if (i + (Senkou_span_B_Period+Shift_Senkou_span_B)) <= len(Data) - 1:
+                Data.loc[(i + (Senkou_span_B_Period+Shift_Senkou_span_B)), 'Senkou-span B'] = (Data.iloc[i:(i + Senkou_span_B_Period), 2].max() + Data.iloc[i:(i + Senkou_span_B_Period),
+                                                                                        3].min()) / 2
+        except ValueError:
+            pass
+    signal(Data, Chikou_span_Period)
+    Data.index = Data['Date']
     Graph(Data)
 
 
+def signal(Data, Chikou_span_Period):
+    for i in range(Chikou_span_Period, len(Data)):
+        if (Data.iloc[i, 6] > Data.iloc[i, 7]) and (Data.iloc[i - 1, 6] < Data.iloc[i - 1, 7]) and (
+                Data.iloc[i, 4] > Data.iloc[i, 9]) and (Data.iloc[i, 4] > Data.iloc[i, 10]) and Data.iloc[i - Chikou_span_Period, 8] > \
+                Data.iloc[i - Chikou_span_Period, 4]:
+            Data.loc[i, 'Buy'] = 1
+        if (Data.iloc[i, 6] < Data.iloc[i, 7]) and (Data.iloc[i - 1, 6] > Data.iloc[i - 1, 7]) and (
+                Data.iloc[i, 4] < Data.iloc[i, 9]) and (Data.iloc[i, 4] < Data.iloc[i, 10]) and Data.iloc[i - Chikou_span_Period, 8] < \
+                Data.iloc[i - Chikou_span_Period, 4]:
+            Data.loc[i, 'Sell'] = 1
+    return Data
+
+
 if __name__ == "__main__":
-    Data = Data.reset_index().reset_index()
-    Data.index = Data['Date']
-    ichimoku(Data)
+    Tenkan_Period = 9
+    Kijun_sen_Period = 26
+    Chikou_span_Period = 26
+    Senkou_span_A_Period = 26
+    Senkou_span_B_Period = 52
+    Shift_Senkou_span_B = 26
+
+    Data = Data.reset_index()
+    Data.rename(columns={'Close': 'Price'}, inplace=True)
+    ichimoku(Data, Tenkan_Period, Kijun_sen_Period, Chikou_span_Period,  Senkou_span_A_Period, Senkou_span_B_Period, Shift_Senkou_span_B)
--- Back-End/graph.py
+++ Back-End/graph.py
@@ -1,17 +1,48 @@
 from datetime import datetime
 from bokeh.plotting import figure, output_file, show
-
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+from bokeh.embed import json_item
+import json
 
 def Graph(Data):
     output_file('graph.html')
-    Data.index = Data['Date']
-    p = figure(title="Ichimoku", x_axis_type="datetime", x_axis_label='Date', y_axis_label='Price')
-    p.line(Data.loc[datetime(2019, 3, 1):datetime.now(), 'Date'],
-           Data.loc[datetime(2019, 3, 1):datetime.now(), 'Price'], legend_label="Price.", line_color="blue",
+    Año = 2010
+    Mes = 3
+    Dia = 1
+    p = figure(plot_width=1400, plot_height=500, title="Ichimoku", x_axis_type="datetime", x_axis_label='Date',
+               y_axis_label='Price')
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Price'], legend_label="Price.", line_color="blue",
            line_width=2)
-    p.line(Data.loc[datetime(2019, 3, 1):datetime.now(), 'Date'],
-           Data.loc[datetime(2019, 3, 1):datetime.now(), 'Tenkan'], legend_label="Tenkan", line_color="red", line_width=2)
-    p.line(Data.loc[datetime(2019, 3, 1):datetime.now(), 'Date'],
-           Data.loc[datetime(2019, 3, 1):datetime.now(), 'Kijun-sen'], legend_label="Kijun-sen", line_color="green",
-           line_width=2)
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Tenkan'], legend_label="Tenkan", line_color="orange",
+           line_dash="4 4")
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Kijun-sen'], legend_label="Kijun-sen", line_color="green",
+           line_dash="4 4")
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Chikou-span'], legend_label="Chikou-span",
+           line_color="purple",
+           line_dash="4 4")
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Senkou-span A'], legend_label="Senkou-span A",
+           line_color="red",
+           line_dash="4 4", line_width=Mes)
+    p.line(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+           Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Senkou-span B'], legend_label="Senkou-span B",
+           line_color="navy",
+           line_dash="4 4", line_width=Mes)
+    p.varea(Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+            Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Senkou-span A'],
+            Data.loc[datetime(Año, Mes, Dia):datetime.now(), 'Senkou-span B'], color='orange')
+    p.asterisk(x=Data[(Data['Buy'] == Dia)].loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+               y=Data[(Data['Buy'] == Dia)].loc[datetime(Año, Mes, Dia):datetime.now(), 'Price'], size=20, color="green",
+               line_width=Mes)
+    p.asterisk(x=Data[(Data['Sell'] == Dia)].loc[datetime(Año, Mes, Dia):datetime.now(), 'Date'],
+               y=Data[(Data['Sell'] == Dia)].loc[datetime(Año, Mes, Dia):datetime.now(), 'Price'], size=20, color="red",
+               line_width=Mes)
+    with open('file.json', 'w', encoding="utf-8") as f:
+        f.write(json.dumps(json_item(p, "myplot")))
     show(p)
